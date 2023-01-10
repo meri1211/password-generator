b7fbf0a388ae9d96291e81cd33700b2b666bc443
meri <mariana_asavoaiei@outlook.com>
2023-01-10 11:26:15 +0000

    created conditional statements to verify the user choices using the response we got from the confirm pop ups

--- script.js
+++ script.js
@@ -90,66 +90,80 @@ var upperCasedCharacters = [
 
 // Declared an array that concatenates the choices of the user for each combination of choices.
 let bigArray = [];
-
+let userPassword = userPasswordOptions
 // Function to prompt user for password options
 function getPasswordOptions() {
   // Created a prompt to ask the user about the length of the password  and stored it in a variable.
-  let lengthOfPassword = prompt(
-    "How many characters should your password have? Min 10- Max 64"
-  );
+   let lengthOfPassword = prompt("How many characters should your password have? Min 10- Max 64");
 
-  if (lengthOfPassword < 10) {
-    alert("Password must be at least 10 characters long!");
-  } else if (lengthOfPassword > 64) {
-    alert("Password cannot be longer than 64 characters!");
-  }
-  console.log(lengthOfPassword);
+   if (lengthOfPassword < 10) {
+     alert("Password must be at least 10 characters long!");
+     
+   }else if (lengthOfPassword > 64) {
+     alert("Password cannot be longer than 64 characters!")
+   }
 
+   console.log(lengthOfPassword);
   // Created confirm pop ups to confirm if the user would like to have  different types of characters, lowercase, etc or not, and stored the    answers in variables.
-  let hasSpecialChar = confirm(
-    "Would you like your password to include special characters?"
-  );
-  console.log(hasSpecialChar);
+   let hasSpecialChar = confirm("Would you like your password to include special characters?");
+   console.log(hasSpecialChar);
 
-  let hasNumbers = confirm("Would you like your password to include numbers?");
-  console.log(hasNumbers);
+   let hasNumbers = confirm("Would you like your password to include numbers?");
+   console.log(hasNumbers);
 
-  let hasLowerCase = confirm(
-    "Would you like your password to have lowercase letters?"
-  );
-  console.log(hasLowerCase);
+   let hasLowerCase = confirm("Would you like your password to have lowercase letters?");
+   console.log(hasLowerCase);
+ 
+   let hasUpperCase = confirm("Would you like your password to have uppercase letters?");
+   console.log(hasUpperCase);
+   // Created an alert in case no character types are chosen.
+   if (hasSpecialChar === false && hasNumbers === false
+     && hasLowerCase === false && hasUpperCase === false) {
+     alert("Must choose at least one character type!")
+   };
 
-  let hasUpperCase = confirm(
-    "Would you like your password to have uppercase letters?"
-  );
-  console.log(hasUpperCase);
-  // Created an alert in case no character types are chosen.
-  if (
-    hasSpecialChar === false &&
-    hasNumbers === false &&
-    hasLowerCase === false &&
-    hasUpperCase === false
-  ) {
-    alert("Must choose at least one character type!");
-  }
+   // Created an object of the options the user chooses.
+   let userPasswordOptions = {
+     length: lengthOfPassword,
+     specialCharacters: hasSpecialChar,
+     containsNumbers: hasNumbers, 
+     lowercase: hasLowerCase, 
+     uppercase: hasUpperCase
+   };
+   console.log(userPasswordOptions);
 
-  // Created an object of the options the user chooses, that we can access with the dot notation.
-  let userPasswordOptions = {
-    length: lengthOfPassword,
-    specialCharacters: hasSpecialChar,
-    containsNumbers: hasNumbers, 
-    lowercase: hasLowerCase, 
-    uppercase: hasUpperCase
-  };
-  
+   // Because the data type returned by the confirm is a boolean (true/ false), we can use conditional statements to concatenate the user options to the big array.
+   if (userPasswordOptions.specialCharacters === true) {
+      bigArray = bigArray.concat(specialCharacters);
+   }
+   if (userPasswordOptions.containsNumbers === true) {
+      bigArray = bigArray.concat(numericCharacters);
+   }
+   if (userPasswordOptions.lowercase === true) {
+      bigArray = bigArray.concat(lowerCasedCharacters);
+   }
+   if (userPasswordOptions.uppercase === true) {
+      bigArray = bigArray.concat(upperCasedCharacters);
+   }
+   console.log(bigArray);
 };
-console.log(userPasswordOptions);
+
+getPasswordOptions();
+
 
 // Function for getting a random element from an array
-function getRandom(arr) {}
+function getRandom(arr) {
+  let randomIndex = Math.floor(Math.random()*bigArray.length);
+    for (let i = 0; i < bigArray.length; i++) {
+      const index = bigArray[i];
+      return randomIndex;
+    }
+}
 
 // Function to generate password with user input
-function generatePassword() {}
+function generatePassword() {
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
